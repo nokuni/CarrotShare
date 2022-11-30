a00413df5886636da32c2d40ca60fee0c844e566
MAERTENS Yann-Christophe <iroshinidako@gmail.com>
2022-11-30 11:03:13 +0100

    Initial Commit

---
+++ CarrotShare/CarrotShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarrotShareApp.swift
+//  CarrotShare
+//
+//  Created by Maertens Yann-Christophe on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CarrotShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
